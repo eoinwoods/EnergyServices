c12b37920f4acbd0abdcdeef2af0417730f7daca
eoinwoods <eoin.woods@artechra.com>
2017-12-31 10:00:39 +0000

    Inital rough commit for Gateway service with example invocations

--- gateway/src/main/java/com/artechra/gateway/Gateway.java
+++ gateway/src/main/java/com/artechra/gateway/Gateway.java
@@ -1,12 +1,38 @@
 package com.artechra.gateway;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.web.client.RestTemplate;
+
 public class Gateway {
+
+
+    RestTemplate restTemplate = new RestTemplate();
+    ObjectMapper mapper = new ObjectMapper() ;
 
     public Gateway(long id) {
 
     }
 
     public String getContent() {
-        return "";
+        callCpuHog(1000) ;
+        callCpuHog(2000) ;
+        return "Called Cpuhog twice (1000, 2000)";
     }
+
+    class Request {
+        public String name ;
+        public Map<String,String> params ;
+
+    }
+
+    private final String CPUHOG_URL = "http://localhost:9001/burncpu?time_msec={time_msec}" ;
+    private String callCpuHog(long timeMsec) {
+        String result = this.restTemplate
+                .getForObject(CPUHOG_URL, String.class, timeMsec + "");
+        return result;
+    }
+
 }
